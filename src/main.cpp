6d3e32b4aad1a8c7c3bb850fe9ca701f8e41b137
enticies <enticies@pop-os.localdomain>
2024-06-13 21:28:52 +0400

    Progress.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "../headers/util.h"
 #include <vector>
 #include <cctype>
+#include <csignal>
 
 using namespace std;
 
@@ -17,6 +18,19 @@ int main() {
         getline(cin, input);
 
         vector<string> parsed_command = parse_command(input);
+
+        std::vector<char*> argv(parsed_command.size() + 1); // +1 for the NULL terminator
+
+        for (size_t i = 0; i < parsed_command.size(); ++i) {
+            argv[i] = const_cast<char*>(parsed_command[i].c_str());
+        }
+
+        argv[parsed_command.size()] = nullptr; // NULL terminator
+
+        if (!parsed_command.empty()) {
+            execvp(argv[0], argv.data());
+            perror("execvp failed");
+        }
     }
 
     return 0;
