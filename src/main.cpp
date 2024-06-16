ed7ba8d21d97b9a57502fb87df5732ef2c6c9ddb
enticies <enticies@pop-os.localdomain>
2024-06-16 22:46:31 +0400

    Progress.

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,12 @@
 #include <vector>
 #include <cctype>
 #include <csignal>
+#include <cstring>
 
 using namespace std;
 
-vector<string> parse_command(const string & command);
-void print_parsed_command(const vector<string> &command);
+vector<const char *> parse_command(const string & command);
+// void print_parsed_command(const vector<string> &command);
 
 int main() {
     string input;
@@ -17,26 +18,26 @@ int main() {
 
         getline(cin, input);
 
-        vector<string> parsed_command = parse_command(input);
+        vector<const char *> parsed_command = parse_command(input);
 
-        std::vector<char*> argv(parsed_command.size() + 1); // +1 for the NULL terminator
+        char ** pointers = new char * [parsed_command.size()];
 
         for (size_t i = 0; i < parsed_command.size(); ++i) {
-            argv[i] = const_cast<char*>(parsed_command[i].c_str());
+            pointers[i] = new char[strlen(parsed_command[i]) + 1];
+            strcpy(pointers[i], parsed_command[i]);
         }
-
-        argv[parsed_command.size()] = nullptr; // NULL terminator
 
         if (!parsed_command.empty()) {
-            execvp(argv[0], argv.data());
+            execvp(pointers[0], pointers);
             perror("execvp failed");
         }
+
     }
 
     return 0;
 }
 
-void print_parsed_command(const vector<string> &command) {
+ void print_parsed_command(const vector<string> &command) {
     string out;
 
     for (int i = 0; i < command.size() - 1; i++) {
@@ -48,8 +49,8 @@ void print_parsed_command(const vector<string> &command) {
     cout << out << endl;
 }
 
-vector<string> parse_command(const string &command) {
-    vector<string> out{};
+vector<const char *> parse_command(const string &command) {
+    vector<const char *> out{};
 
     string currentString;
 
@@ -59,13 +60,13 @@ vector<string> parse_command(const string &command) {
         }
         else {
             if (!currentString.empty()) {
-                out.push_back(currentString);
+                out.push_back(currentString.c_str());
             }
             currentString = "";
         }
     }
 
-    out.push_back(currentString);
+    out.push_back(currentString.c_str());
 
     return out;
 }
