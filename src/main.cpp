6ac1755e1d1bcd88b43c187067d7a13475338d4f
enticies <enticies@pop-os.localdomain>
2024-06-19 22:42:05 +0400

    Progress.

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,21 @@
 #include <cctype>
 #include <csignal>
 #include <cstring>
+#include <sys/wait.h>
+#include <filesystem>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+
+
 
 using namespace std;
+using namespace filesystem;
 
-vector<const char *> parse_command(const string & command);
+vector<string> parse_command(const string &command);
 // void print_parsed_command(const vector<string> &command);
+char ** convert_vector_to_array(vector<string> &parsed_command);
+void change_directory(char ** path);
 
 int main() {
     string input;
@@ -18,26 +28,64 @@ int main() {
 
         getline(cin, input);
 
-        vector<const char *> parsed_command = parse_command(input);
+        vector<string> parsed_command = parse_command(input);
+        char ** parsed_command_array = convert_vector_to_array(parsed_command);
 
-        char ** pointers = new char * [parsed_command.size()];
+        int pid = fork();
 
-        for (size_t i = 0; i < parsed_command.size(); ++i) {
-            pointers[i] = new char[strlen(parsed_command[i]) + 1];
-            strcpy(pointers[i], parsed_command[i]);
+        if (pid < 0) {
+            perror("Failed to fork the process.");
+        }
+        else if (pid == 0) {
+            if (*parsed_command_array) {
+                if (parsed_command[0] == "cd") {
+                    change_directory(++parsed_command_array);
+                }
+                else {
+                    int exec_result = execvp(*parsed_command_array, parsed_command_array);
+
+                    if (exec_result == -1) {
+                        perror("Failed to execute the process");
+                    }
+                }
+
+            }
         }
 
-        if (!parsed_command.empty()) {
-            execvp(pointers[0], pointers);
-            perror("execvp failed");
-        }
+        wait(nullptr);
 
     }
 
     return 0;
 }
 
- void print_parsed_command(const vector<string> &command) {
+void change_directory(char **p) {
+    struct passwd *pw = getpwuid(getuid());
+
+    const char *homedir = pw->pw_dir;
+
+    if (*p == nullptr) {
+        cout << "GOING TO HOME!" << endl;
+        current_path(path(homedir));
+    }
+    else {
+        cout << "GOING TO PATH!" << endl;
+        current_path(path(*p));
+    }
+
+}
+
+char ** convert_vector_to_array(vector<string> &parsed_command) {
+    char ** out = new char * [parsed_command.size()];
+
+    for (size_t i = 0; i < parsed_command.size(); ++i) {
+        out[i] = parsed_command[i].data();
+    }
+
+    return out;
+}
+
+void print_parsed_command(const vector<string> &command) {
     string out;
 
     for (int i = 0; i < command.size() - 1; i++) {
@@ -49,8 +97,9 @@ int main() {
     cout << out << endl;
 }
 
-vector<const char *> parse_command(const string &command) {
-    vector<const char *> out{};
+// refactor so that this returns an array
+vector<string> parse_command(const string &command) {
+    vector<string> out{};
 
     string currentString;
 
